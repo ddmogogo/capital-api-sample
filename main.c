146228ef801075ee55b7f8b3597b481bcc48a242
leolarrel <leolarrel@localhost>
2012-09-13 15:06:55 +0800

    add min window

---
+++ main.c
@@ -0,0 +1,82 @@
+#include <windows.h>
+#include <stdio.h>
+#include "quoteapi.h"
+
+const char g_szClassName[] = "quoteview";
+
+// Step 4: the Window Procedure
+LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam);
+LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+    switch(msg)
+    {
+        case WM_CREATE:
+            printf("message %x\n",msg);
+            QL_LoginServer(/*"ID","Password"*/); //this line will compile fail. that is tip
+            QL_Request(/*""*/);//this line will compile fail. that is tip
+            break;
+
+        case WM_CLOSE:
+            QL_Bye();
+            DestroyWindow(hwnd);
+            break;
+
+        case WM_DESTROY:
+            PostQuitMessage(0);
+            break;
+
+        default:
+            return DefWindowProc(hwnd, msg, wParam, lParam);
+    }
+    return 0;
+}
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,LPSTR lpCmdLine, int nCmdShow);
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,LPSTR lpCmdLine, int nCmdShow)
+{
+    WNDCLASSEX wc;
+    HWND hwnd;
+    MSG Msg;
+
+    //Step 1: Registering the Window Class
+    wc.cbSize        = sizeof(WNDCLASSEX);
+    wc.style         = 0;
+    wc.lpfnWndProc   = WndProc;
+    wc.cbClsExtra    = 0;
+    wc.cbWndExtra    = 0;
+    wc.hInstance     = hInstance;
+    wc.hIcon         = LoadIcon(NULL, IDI_APPLICATION);
+    wc.hCursor       = LoadCursor(NULL, IDC_ARROW);
+    wc.hbrBackground = (HBRUSH)(COLOR_WINDOW+1);
+    wc.lpszMenuName  = NULL;
+    wc.lpszClassName = g_szClassName;
+    wc.hIconSm       = LoadIcon(NULL, IDI_APPLICATION);
+
+    if(!RegisterClassEx(&wc))
+    {
+        printf("Window Registration Failed! Job aborted\n");
+        return 0;
+    }
+
+    // Step 2: Creating the Window
+    hwnd = CreateWindowEx(WS_EX_CLIENTEDGE,"quoteview","The title of my window",
+            WS_OVERLAPPEDWINDOW,CW_USEDEFAULT,CW_USEDEFAULT,20,20,NULL, NULL, hInstance, NULL);
+
+    if(hwnd == NULL)
+    {
+        printf("Window Creation Failed! Job aborted\n");
+        return 0;
+    }
+
+    ShowWindow(hwnd, nCmdShow);
+    UpdateWindow(hwnd);
+
+    // Step 3: The Message Loop
+    while(GetMessage(&Msg, NULL, 0, 0) > 0)
+    {
+        TranslateMessage(&Msg);
+        DispatchMessage(&Msg);
+    }
+    return Msg.wParam;
+}
+
